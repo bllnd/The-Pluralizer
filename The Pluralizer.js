ff5d09b4f5ff49d0baee2bb08114887ba7416a25
bllnd <bllnd@mac.com>
2015-08-31 09:13:21 -0700

    The Pluralizer

---
+++ The Pluralizer.js
@@ -0,0 +1,15 @@
+var noun = "goose";
+var number = 99;
+
+if (noun == "cat"){
+  console.log(number+" cats");
+}
+else if(noun == "dog"){
+  console.log(number+" dogs");
+}
+else if(noun == "sheep"){
+  console.log(number+" sheep");
+}
+else if (noun == "goose"){
+  console.log(number+" geese");
+}
